6bbef44c849266af30160745ae892593c7409bfa
Ed Jethro O Pacaño <orhtej14@gmail.com>
2021-11-03 14:30:46 +0800

    minor changes

--- test2.py
+++ test2.py
@@ -1,13 +1,9 @@
 import detechYolo
-import threading
 
-test = detechYolo.Detech("best.pt", "0", 640, 'cpu', "webcam", [0,1,2,3])
+test = detechYolo.Detech("epoch_250.pt", "0", 640, 'cpu', "webcam")
+test.show_res = True
+# test.startInference()
 test.loadModel()
 test.loadData()
-
-# thread1 = threading.Thread(target=test.loadModel, daemon=True)
-# thread2 = threading.Thread(target=test.loadData, daemon=True)
-thread3 = threading.Thread(target=test.startInference, daemon=False)
-# thread1.start()
-# thread2.start()
-thread3.start()
+test.isDetecting = True
+test.runInference()
